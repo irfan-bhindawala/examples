c021b80d5afe05fd61c9b9acda8e38c3fea94054
Bhindawala, Irfan <irfan.bhindawala@mastercard.com>
2018-09-22 21:34:50 +0530

    application starter created

---
+++ learn-effective-java/src/main/java/com/techzone/learn/effective/java/ApplicationRunner.java
@@ -0,0 +1,8 @@
+package com.techzone.learn.effective.java;
+
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        /* just initial commit that's why it is empty moving further an implementation will be there*/
+    }
+}
